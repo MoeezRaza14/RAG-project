25d6de15f4277cde4d2082cc000ee47b64a429e7
MoeezRaza14 <155905342+MoeezRaza14@users.noreply.github.com>
2025-12-17 12:05:57 +0500

    Delete get_embeddings.py

--- get_embeddings.py
+++
@@ -1,7 +0,0 @@
-from langchain_huggingface import HuggingFaceEmbeddings
-
-def get_embedding_function():
-    embeddings = HuggingFaceEmbeddings
-    model_name = "Qwen/Qwen3-Embedding-0.6B",
-    model_kwargs= {"device": "cpu"}
-    return embeddings
